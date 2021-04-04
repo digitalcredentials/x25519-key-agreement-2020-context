794ab9b08be618ddc2bddb8dd4c2f93fadbd718f
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-04-03 22:28:44 -0400

    Fix context file extension.

--- js/documentLoader.js
+++ js/documentLoader.js
@@ -4,7 +4,7 @@
 'use strict';
 
 const {CONTEXT_URL} = require('./constants');
-const context = require('../contexts/x25519-key-agreement-2020-v1.json');
+const context = require('../contexts/x25519-key-agreement-2020-v1.jsonld');
 
 module.exports = {
   documentLoader(url) {
